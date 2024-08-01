1c0994f7075fb87600c056401cefd5a07888a2f6
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2024-07-31 20:58:36 -0700

    fixed issue with not going to login screen after logout

--- src/app/components/UserInfo.jsx
+++ src/app/components/UserInfo.jsx
@@ -17,7 +17,7 @@ export default function UserInfo() {
           Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
         <button
-          onClick={() => signOut()}
+          onClick={() => signOut({ callbackUrl: '/' })}
           className="bg-red-500 text-white font-bold px-6 py-2 mt-3 rounded-md"
         >
           Log Out
